3195fa2e3ba4e8450f8e9a4039c4a8274daa075d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-07-16 10:43:51 +0200

    Sample hook with extern npm module
    - http default webhook
    - lodash.upperFirst module
    - @types/lodash

---
+++ HttpTriggerJS/index.js
@@ -0,0 +1,18 @@
+const upperFirst = require('lodash.upperfirst');
+
+module.exports = (context, req) => {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: `Hello ${upperFirst(req.query.name || req.body.name)}`,
+        };
+    } else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
